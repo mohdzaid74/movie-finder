1489307458ee6d4104a0b1fb04bba26a7e844b3d
mzaid2025 <mz8059861@gmail.com>
2026-09-12 15:57:03 +0530

    remove movie logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="header">
-      <div className="logo">🎬 MovieFinder</div>
+      <div className="logo">MovieFinder</div>
 
       <Link className="nav-link" to="/">
         Home
